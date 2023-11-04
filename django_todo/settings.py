c5de8ce85f26d13a7588ec6c2fd64485c446691c
Shiimymy <>
2023-11-04 11:36:15 +0000

    Fix allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-w%i5zvvj@^1(7%^hi4_@u=*s+5@fm9d-vz^jvrf-*_1+u-c8is
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['django-tasks-manager-3322759fd3df.herokuapp.com']
-ALLOWED_HOSTS = ['8000-shiimymy-hello-django-ev9fqudwgj.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['django-tasks-manager-c052e39c8b70.herokuapp.com']
+# ALLOWED_HOSTS = ['8000-shiimymy-hello-django-ev9fqudwgj.us2.codeanyapp.com']
 
 # Application definition
 
